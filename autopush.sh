b64fd2819358f02e7ab4ed716c99d3ed76dfaae8
yuqi <qiyuqi1995@gmail.com>
2019-07-19 13:57:05 +0900

    [autopush] fix autopush

--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,8 @@
+echo "\n==========GIT STATUS==========\n"
 git status
+echo "\n==========GIT ADD=============\n"
 git add .
+echo "\n==========GIT COMMIT==========\n"
 git commit -m "[autopush] $1"
+echo "\n==========GIT PUSH============\n"
 git push
